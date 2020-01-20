bfdfa173f8f2741da3416f356aab9c5b8d35abcc
Jim Segal <hello@jimsegal.com>
2020-01-20 16:39:10 -0600

    only add if it's not already added, store passphrase in keychain

--- personal-git.sh
+++ personal-git.sh
@@ -3,11 +3,10 @@
 # per this, https://gist.github.com/jexchan/2351996
 # when I restart my computer, I need to reassociate my personal github ssh key
 
-## TODO make this idempotent
-## grep ssh-add -l for jsegal205
 ## add task to run this on startup.
-## store passphrase in keychain so I don't have to type it.
 
-ssh-add ~/.ssh/id_rsa_jsegal205
-
-# enter passphrase
+echo ${ssh-add -l} | grep --quiet "jsegal205"
+if [ $? = 1 ]; then
+  # 1 === the echo didn't return anything
+  ssh-add -K ~/.ssh/id_rsa_jsegal205 # -K to save password in keychain
+fi
