7390ba8bd05513a3580973dd2090c72f5f67db93
Jim Segal <hello@jimsegal.com>
2020-01-20 17:14:14 -0600

    update comment on how to run on login

--- personal-git.sh
+++ personal-git.sh
@@ -3,7 +3,8 @@
 # per this, https://gist.github.com/jexchan/2351996
 # when I restart my computer, I need to reassociate my personal github ssh key
 
-## add task to run this on startup.
+# use Automator to run shell script on login
+# https://stackoverflow.com/questions/6442364/running-script-upon-login-mac
 
 echo ${ssh-add -l} | grep --quiet "jsegal205"
 if [ $? = 1 ]; then
